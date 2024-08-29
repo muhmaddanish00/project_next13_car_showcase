b0b26093b8fcb92116f6e4c890ea1b47d32befb7
muhmaddanish00 <140235382+muhmaddanish00@users.noreply.github.com>
2024-08-29 09:45:08 +0100

    Your commit message here

--- utils/index.ts
+++ utils/index.ts
@@ -46,8 +46,8 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    	'x-rapidapi-key': '7a21d3d04dmsh7d4191b24609904p1bbc89jsn10ed44da0571',
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
   };
 
   // Set the required headers for the API request
